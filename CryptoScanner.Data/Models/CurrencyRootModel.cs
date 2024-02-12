e441b19991a8208875b31ca9d673de6e401b9423
JonhannesNilsson <jonte9203@hotmail.com>
2024-02-12 15:37:34 +0100

    added root models

---
+++ CryptoScanner.Data/Models/CurrencyRootModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoScanner.Data.Models
+{
+	public class CurrencyRootModel
+	{
+		[JsonProperty("id")]
+		public string? Id { get; set; }
+
+		[JsonProperty("symbol")]
+		public string? Symbol { get; set; }
+
+		[JsonProperty("name")]
+		public string? Name { get; set; }
+	}
+
+	public class SingleRootModel
+	{
+		[JsonProperty("sek")]
+		public int? Sek { get; set; }
+	}
+
+}
