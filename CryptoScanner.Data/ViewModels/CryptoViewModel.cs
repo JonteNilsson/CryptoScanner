76fe4780eb2367f2d7d2abc43affa60e43aabe78
PaulineI <pauline.ibraim@gmail.com>
2024-02-14 09:56:35 +0100

    ViewModels

---
+++ CryptoScanner.Data/ViewModels/CryptoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoScanner.Data.ViewModels
+{
+    public class CryptoViewModel
+    {
+        public string Name { get; set; }
+
+        public double ValueinSek { get; set; }
+    }
+}
